5d537df9676a9379f9612f727192f952cb2b14fd
njagadeesan <nandhakumar.jagadeesan@nextsphere.com>
2020-05-10 04:10:30 +0530

    added icons

--- GroceryShop/src/app/modules/shopping-cart/cart-details/cart-details.component.ts
+++ GroceryShop/src/app/modules/shopping-cart/cart-details/cart-details.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cart-details',
   templateUrl: './cart-details.component.html',
-  styleUrls: ['./cart-details.component.css']
+  styleUrls: ['./cart-details.component.less']
 })
 export class CartDetailsComponent implements OnInit {
 
